d3f16db71c08a66f92c2826486e10028208db28b
Javin Vora <javinvora@Javins-MacBook-Air.local>
2023-01-11 18:40:26 -0700

    done 2

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 import requests
 
 print(requests.__version__)
-response = requests.get("http://google.com/");
+response = requests.get("https://raw.githubusercontent.com/Javinvora/404Lab1/master/lab1.py");
 print(response.text)
